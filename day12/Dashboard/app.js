bc59380c582e9fa5d77eb5e1ca812a38c3f7e467
Balaji Bharath (Annalect) <balaji.bharath@annalect.com>
2024-11-22 19:39:09 +0530

    working on day 12 dashboard

---
+++ day12/Dashboard/app.js
@@ -0,0 +1,30 @@
+
+const menuBtn = document.getElementById('menu-btn');
+const closeBtn = document.getElementById('close-btn');
+const sidebar = document.getElementById('sidebar');
+const productBtn = document.getElementById('product-btn');
+const orderBtn = document.getElementById('order-btn');
+
+
+menuBtn.addEventListener('click', () => {
+    sidebar.style.left = '0px'; 
+});
+
+
+closeBtn.addEventListener('click', () => {
+    sidebar.style.left = '-250px'; 
+});
+
+
+productBtn.addEventListener('click', () => {
+    navigateTo('./day1/Problem_Statement1/index.html'); t
+});
+
+
+orderBtn.addEventListener('click', () => {
+    navigateTo('./day7/Problem_Statement2/index.html'); 
+});
+
+function navigateTo(path) {
+    window.location.href = path;
+}
